3230227fe677a5786edfb4fd8570f3ba8bb1d609
priyadharshinirathinam <rpriyadharshini26@gmail.com>
2019-11-07 14:05:58 +0530

    i have done the user stories

---
+++ src/org/test/array/sampleexe.java
@@ -0,0 +1,18 @@
+package org.test.array;
+
+public class sampleexe {
+
+		public void main(String[] args) {      
+			 
+				int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+				int sum = 0;
+
+				for (int i=0;i<=a.length;i++)
+				    sum=sum + i;
+				System.out.println("The sum is " + sum);
+				
+				
+		}
+		}
+	
+
